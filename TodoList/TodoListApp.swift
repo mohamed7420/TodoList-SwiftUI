22773d3f175c22fd479f62d8f40877e6d3ed803e
mohamed osama <mohamed.osama9812@gmail.com>
2024-01-26 17:45:39 +0200

    Initial Commit

---
+++ TodoList/TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by Mohamed Osama on 26/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
